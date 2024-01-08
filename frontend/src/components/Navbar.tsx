8cbf6584b98ae51c4a7c60880edafa86a9d86b88
[Simon] <[simon.sandevik@outlook.com]>
2024-01-08 19:51:38 +0100

    add icons

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,17 @@
 import Link from 'next/link'
 import React from 'react'
+import { MdDashboard } from "react-icons/md";
+import { FiLogIn } from "react-icons/fi";
+import { IoMdHelp } from "react-icons/io";
 
 export default function Navbar() {
   return (
     <nav className="h-[3em] bg-[var(--blue)] z-10 flex justify-between items-center px-4">
         <span className="font-bold text-2xl">CRM</span>
         <ul className='flex gap-8 items-center '>
-            <li>Dashboard</li>
-            <li>Help</li>
-            <li><Link href={"/sign-in"}>Sign in</Link></li>
+            <li><Link href={"/dashboard"} className='flex items-center gap-1 text-[var(--dark-green)] hover:text-[var(--pink)] transition-colors'> <MdDashboard /> Dashboard </Link></li>
+            <li className='flex items-center gap-1 text-[var(--dark-green)] hover:text-[var(--pink)] transition-colors'>Help <IoMdHelp /></li>
+            <li><Link href={"/sign-in"} className='flex items-center gap-1 text-[var(--dark-green)] hover:text-[var(--pink)] transition-colors'> Sign in <FiLogIn /> </Link></li>
         </ul>
     </nav>
   )
