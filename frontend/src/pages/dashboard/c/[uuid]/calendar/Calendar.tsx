e8dc4dab97ca6b0e25b4848212948c726c3ea6b3
[Simon] <[simon.sandevik@outlook.com]>
2024-01-19 17:57:04 +0100

    change dates

--- frontend/src/pages/dashboard/c/[uuid]/calendar/Calendar.tsx
+++ frontend/src/pages/dashboard/c/[uuid]/calendar/Calendar.tsx
@@ -1,39 +1,49 @@
 import Button from '@/components/Button';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Calendar() {
 
-    const [currentDate, setCurrentDate] = useState<Date>(new Date());
-    const year = new Date().getFullYear();
-    const month = new Date().getMonth() + 1;
-    const date = new Date().getDate();
-
+    const currentDate = new Date();
+    const [activeDate, setActiveDate] = useState<Date>(new Date());
+    const [meetings, setMeetings] = useState<Meeting[]>([]);
 
     const numDaysThisMonth = (y: number, m: number) => new Date(y, m, 0).getDate();
 
     const nextMonth = () => {
-        setCurrentDate(new Date(currentDate.getMonth() == 11 ? currentDate.getFullYear() + 1 : currentDate.getFullYear(), currentDate.getMonth() === 11 ? 0 : currentDate.getMonth() + 1))
+        setActiveDate(new Date(activeDate.getMonth() == 11 ? activeDate.getFullYear() + 1 : activeDate.getFullYear(), activeDate.getMonth() === 11 ? 0 : activeDate.getMonth() + 1))
     }
     const prevMonth = () => {
-        setCurrentDate(new Date(currentDate.getMonth() == 0 ? currentDate.getFullYear() - 1 : currentDate.getFullYear(), currentDate.getMonth() === 0 ? 11 : currentDate.getMonth() - 1))
+        setActiveDate(new Date(activeDate.getMonth() == 0 ? activeDate.getFullYear() - 1 : activeDate.getFullYear(), activeDate.getMonth() === 0 ? 11 : activeDate.getMonth() - 1))
     }
 
+
+    const getMeetings = async () => {
+
+    }
+
+    useEffect(()=>{
+        console.log("new month")
+    },[activeDate])
+
+
+
+    const arr = Array(numDaysThisMonth(activeDate.getFullYear(), activeDate.getMonth() + 1)).fill("");
 
   return (
     <div className='max-w-[1440px] m-auto p-4 flex flex-col gap-4'>
 
         <div className="flex justify-center items-center gap-4">
             <Button onClick={() => prevMonth()}>Prev</Button>
-            {currentDate.getFullYear() + " " + matchMonth(currentDate.getMonth() + 1) }
+            {activeDate.getFullYear() + " " + matchMonth(activeDate.getMonth() + 1) }
             <Button onClick={() => nextMonth()}>Next</Button>
         </div>
 
         <ul className="grid grid-cols-auto-fill md:grid-cols-calendar gap-2 ">
-            {Array(numDaysThisMonth(currentDate.getFullYear(), currentDate.getMonth() + 1)).fill("a").map((_, i) => (
-            <li className={`h-32  hover:bg-light-purple transition-colors ${i+1 === date ? "bg-light-purple" : i + 1 < date ? "bg-background-light bg-opacity-50" : "bg-background-light" }`} key={i + 1}>
+            {arr.map((_, i) => (
+                <li className={`h-32  hover:bg-light-purple transition-colors ${new Date(activeDate.getFullYear(), activeDate.getMonth(), i + 1).toDateString() === currentDate.toDateString() ? "bg-light-purple" : new Date(activeDate.getFullYear(), activeDate.getMonth(), i + 1).getTime() < currentDate.getTime() ? "bg-background-light bg-opacity-50" : "bg-background-light" }`} key={i + 1}>
                 {i + 1}
                 <br />
-                {matchWeekDay(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, i + 1).getDay())}
+                {matchWeekDay(new Date(activeDate.getFullYear(), activeDate.getMonth() + 1, i + 1).getDay())}
             </li>
             ))}
         </ul>
