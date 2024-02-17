2227b34c3eeafd2583b24e02e231296065572da0
[Simon] <[simon.sandevik@outlook.com]>
2024-02-17 15:11:02 +0100

    fix Loading bar

--- frontend/src/pages/dashboard/c/[uuid]/clients/[clientUuid]/Task.tsx
+++ frontend/src/pages/dashboard/c/[uuid]/clients/[clientUuid]/Task.tsx
@@ -3,7 +3,7 @@ import { FaTriangleExclamation } from "react-icons/fa6";
 
 
 export default function Task({task}: {task: Task}) {
-  const percentage:number = (100-(task?.percentage || 100)) > 100 ? 100 : (100-(task?.percentage || 100)) < 0 ? 0 : (100-(task?.percentage || 100));
+  const percentage:number = (task?.percentage || 100) > 100 ? 100 : (task?.percentage || 100) < 0 ? 0 : (task?.percentage || 100);
 
   return (
     <li className='min-w-40 rounded-sm bg-background-light p-2 capitalize relative overflow-hidden h-11 hover:h-20 transition-all flex justify-between items-center gap-6'>
@@ -17,7 +17,7 @@ export default function Task({task}: {task: Task}) {
 const LoadingBar = ({percentage}: {percentage: number}) => {
   return (
     <div className={`absolute bottom-2 h-1 bg-white w-full -m-2 `}>
-      <div style={{"--percentage": percentage.toString()+"%"} as any} className={`h-1 w-full ${percentage === 100 ? "rounded-r-md bg-light-red left-0" : "rounded-none bg-blue-500 left-[-100%] translate-x-[var(--percentage)]"} absolute bottom-0  `}></div>
+      <div style={{"--percentage": percentage.toString()+"%"} as any} className={`h-1 w-full ${percentage === 100 ? "rounded-r-md bg-light-red left-0" : "rounded-none bg-blue-500 left-0 translate-x-[calc(-100%+var(--percentage))]"} absolute bottom-0  `}></div>
     </div>
   )
 }
