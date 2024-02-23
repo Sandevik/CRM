b06be73d78b1943a5907baa503e607d09b0096f9
[Simon] <[simon.sandevik@outlook.com]>
2024-02-23 23:21:08 +0100

    layout

--- crm/src/pages/dashboard/c/[uuid]/customers/[customerUuid]/index.tsx
+++ crm/src/pages/dashboard/c/[uuid]/customers/[customerUuid]/index.tsx
@@ -114,10 +114,10 @@ export default function index() {
   },[currentView])
 
   return (
-      <div className='relative px-2'>
+      <div className='relative w-full pl-3 pr-2 '>
         <Navbar />
-        <Link href={`/dashboard/c/${crm?.crmUuid}/customers`} className="flex gap-2 items-center text-lg bg-light-blue hover:bg-greenish transition-colors absolute top-[3.3em] left-1 px-2 text-black rounded-md"><FaChevronLeft /> <div><Text text={{eng: "Customers", swe: "Kunder"}} /></div> </Link>
-        <main className='h-[calc(100dvh-9.5em)] rounded-md w-full mt-12 p-4 bg-background-light bg-opacity-50 flex flex-col lg:flex-row gap-4 overflow-y-scroll scrollthumb'>
+        <Link href={`/dashboard/c/${crm?.crmUuid}/customers`} className="flex gap-2 items-center text-lg bg-light-blue hover:bg-greenish transition-colors absolute top-[2.7em] left-3 px-2 text-black rounded-md"><FaChevronLeft /> <div><Text text={{eng: "Customers", swe: "Kunder"}} /></div> </Link>
+        <main className='h-[calc(100dvh-9.1em)] rounded-md w-full mt-10 p-4 bg-background-light bg-opacity-50 flex flex-col lg:flex-row gap-4 overflow-y-scroll scrollthumb'>
           <CustomerCard customer={customer} setEdit={setEdit} edit={edit}/>
           <div className="flex-1 w-full relative">
 
